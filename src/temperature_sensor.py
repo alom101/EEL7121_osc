0aa1112e6653e5914362a10c354787e778206a54
Arthur Osorio <osorioarthur@gmail.com>
2025-10-07 21:13:41 -0300

    temperature_sensor.py: basic code done

--- src/temperature_sensor.py
+++ src/temperature_sensor.py
@@ -1,20 +1,43 @@
-from machine import ADC
+from machine import Pin, ADC
+from time import ticks_us
 
-class TemperatureSensor:
-    def __init__(self, pin, a, b):
-        self.pin = ADC(pin)
-        self.a = a
-        self.b = b
+
+class SensorInterface:
+    def read(self):
+        raise NotImplementedError()
+
+
+class SensorADC:
+    def __init__(self, pin):
+        self.adc = ADC(Pin(pin))
 
     def read(self):
-        return self.pin.read_u16() #number from 0 to 65535
-    
-    def read_voltage(self):
-        return self.pin.read_u16()*3.3/65535
-
-    def read_temperature(self):
-        return self.a*self.read()+self.b
+        return self.adc.read_u16()
 
 
+class SensorTimeConstant:
+    def __init__(self, pin):
+        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
+        self.t_up = ticks_us()
+        self.t_down = ticks_us()
+        self.pin.irq(handler=self.on_pin_change, trigger=Pin.IRQ_RISING|Pin.IRQ_FALLING)
+
+    def on_pin_change(self, pin):
+        if pin():
+            print("UP")
+            self.t_up = ticks_us()
+        else:
+            print("DOWN")
+            self.t_down = ticks_us()
+
+    def read(self):
+        return abs(self.t_up - self.t_down)
 
 
+if __name__=="__main__":
+    from time import sleep
+    s = SensorTimeConstant(24)
+
+    while(True):
+        print(s.read())
+        sleep(1)
