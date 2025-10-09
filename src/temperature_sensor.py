2f6276585f04ffe8a74158b687b41662714e63c8
Arthur Osorio <osorioarthur@gmail.com>
2025-10-08 23:02:45 -0300

    temperature_sensor.py: SensorADC e SensorTimeConstant tested!

--- src/temperature_sensor.py
+++ src/temperature_sensor.py
@@ -7,7 +7,7 @@ class SensorInterface:
         raise NotImplementedError()
 
 
-class SensorADC:
+class SensorADC(SensorInterface):
     def __init__(self, pin):
         self.adc = ADC(Pin(pin))
 
@@ -15,26 +15,34 @@ class SensorADC:
         return self.adc.read_u16()
 
 
-class SensorTimeConstant:
-    def __init__(self, pin):
-        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
-        self.t_up = ticks_us()
-        self.t_down = ticks_us()
-        self.pin.irq(handler=self.on_pin_change, trigger=Pin.IRQ_RISING|Pin.IRQ_FALLING)
+class SensorTimeConstant(SensorInterface):
+    def __init__(self, input_pin, output_pin):
+        self.input_pin = Pin(input_pin, Pin.IN, Pin.PULL_UP)
+        self.output_pin = Pin(output_pin, Pin.OUT)
+        self.output_pin(False)
+        self.t_up = 0
+        self.t_down = 0
+        self.t_delta = 0
+        self.input_pin.irq(handler=self.on_pin_change, trigger=Pin.IRQ_RISING|Pin.IRQ_FALLING)
+        self.output_pin(True)
 
     def on_pin_change(self, pin):
         if pin():
             self.t_up = ticks_us()
+            self.output_pin(False)
         else:
             self.t_down = ticks_us()
+            self.output_pin(True)
+            self.t_delta = abs(self.t_up - self.t_down)
 
     def read(self):
-        return abs(self.t_up - self.t_down)
+        return self.t_delta
 
 
 if __name__=="__main__":
     from time import sleep
-    s = SensorTimeConstant(24)
+    # s = SensorTimeConstant(input_pin=14, output_pin=15)
+    s = SensorADC(26)
 
     while(True):
         print(s.read())
