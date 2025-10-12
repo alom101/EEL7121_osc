c0b10050c74c6e27d299604d755047c9c4d38577
Arthur Osorio <osorioarthur@gmail.com>
2025-10-12 13:00:07 -0300

    Thermistor calibration: jupyter notebook done

--- src/thermistor_calibration.py
+++ src/thermistor_calibration.py
@@ -1,18 +1,46 @@
 from temperature_sensor import SensorDS18B20, SensorThermistorRseries
-from time import sleep, gmtime
+from time import sleep, time
+from machine import Pin
+from os import listdir
 
-PERIOD = 1 # seconds
+PERIOD = 10  # seconds
+BASE_NAME = "calibration_data_"
 
 sensor_reference = SensorDS18B20(4)
-sensor_thermistor= SensorThermistorRseries(27, 10_000, 1,1)
+sensor_thermistor = SensorThermistorRseries(27, 10_000, 1, 1)
+led_builtin = Pin(25, Pin.OUT)
 
-with open("calibration_data.tsv", "wt") as file:
-    file.write("time\ttemperature\tresistance\n")
-    while True:
-        t = gmtime()
-        T = sensor_reference.read()
-        R = sensor_thermistor.temperature_to_resistance(sensor_thermistor.read())
-        str = f"{t}\t{T}\t{R}\n"
+
+# get new file_name
+i = 1
+file_name = BASE_NAME + str(i) + ".tsv"
+while file_name in listdir():
+    i += 1
+    file_name = BASE_NAME + str(i) + ".tsv"
+print(f"Writing to {file_name}")
+
+# set header
+header = "time\ttemperature\tresistance\n"
+with open("calibration_data.tsv", "at") as file:
+    file.write(header)
+print(header, end='')
+
+# wait SensorDS18B20 setup time
+while sensor_reference.read() == 0:
+    sleep(0.1)
+
+# begin recording
+t0 = time()
+while True:
+    led_builtin.on()
+    t = time()-t0
+    T = sensor_reference.read()
+    R = sensor_thermistor.temperature_to_resistance(
+            sensor_thermistor.read()
+        )
+    str = f"{t}\t{T}\t{R}\n"
+    with open(file_name, "at") as file:
         file.write(str)
-        print(str, end='')
-        sleep(PERIOD)
+    print(str, end='')
+    led_builtin.off()
+    sleep(PERIOD)
