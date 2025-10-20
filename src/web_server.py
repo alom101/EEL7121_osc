8d9e4f901c777a349285574b341078aac0c8502a
Arthur Osorio <osorioarthur@gmail.com>
2025-10-20 19:21:06 -0300

    web server working

--- src/web_server.py
+++ src/web_server.py
@@ -1,11 +1,15 @@
 import network
 import socket
-import sys
-import json
-from time import sleep, ticks_us
-from machine import Pin, ADC, Timer, PWM
-from math import log, exp, sqrt
-from rp2 import StateMachine, asm_pio
+from machine import Pin
+
+from temperature_sensor import SensorThermistorRseriesV2
+from heater import HeaterPWM
+from strategy import StrategyOnOff
+from thermal_controller import ThermalController
+from frequency_sensor import FreqSensorPIO
+from tests import Clock
+
+
 
 ssid = 'Andrio2'
 password = 'ufsc202502'
@@ -14,141 +18,20 @@ password = 'ufsc202502'
 HEATER_PWM_PIN = 15      
 THERMISTOR_ADC_PIN = 27
 FREQUENCY_PIN = 26       
-
+FAKE_OSCILATOR_PIN = 22
 
 R_SERIES = 10_000
-
-A_TERM = 38.701
-B_TERM = -8.882
 
 TARGET_TEMP_INITIAL = 50.0 
 KP = 500
 KI = 0.1
 UPDATE_FREQ_HZ = 10
 
-from temperature_sensor import SensorThermistorRseriesV2
-from heater import HeaterPWM
-from strategy import StrategyOnOff
-from thermal_controller import ThermalController
-from frequency_sensor import FreqSensorPIO
-
-# class TempSensorInterface:
-#     def read(self):
-#         raise NotImplementedError()
-#
-# class SensorThermistorRseries(TempSensorInterface):
-#     # logR = A + B*log(R)
-#     def __init__(self, adc_pin, r_series, A, B):
-#         self.adc = ADC(Pin(adc_pin))
-#         self.r_series = r_series
-#         self.A = A
-#         self.B = B
-#
-#     def resistance_to_temperature(self, resistance):
-#         return self.A + self.B*log(resistance)
-#
-#     def adc_to_resistance(self, adc_read):
-#         return (self.r_series*(2**16-1))/adc_read - self.r_series
-#
-#     def read(self):
-#         try:
-#             r = self.adc_to_resistance(self.adc.read_u16())
-#             temp = self.resistance_to_temperature(r)
-#             return round(temp, 2)
-#         except Exception:
-#             return 0.0
-#
-#
-# class HeaterInterface:
-#     def read(self) -> int:
-#         raise NotImplementedError()
-#     def write(self, value: int):
-#         raise NotImplementedError()
-#
-# class HeaterPWM(HeaterInterface):
-#     def __init__(self, pin, pwm_freq=60, initial_value=0, invert=False):
-#         self.pwm = PWM(Pin(pin, Pin.OUT), freq=pwm_freq, duty_u16=initial_value, invert=invert)
-#     def read(self) -> int:
-#         return self.pwm.duty_u16()
-#     def write(self, value: int):
-#         self.pwm.duty_u16(int(value))
-#
-#
-# class StrategyInterface:
-#     def update(self, current_value, target):
-#         raise NotImplementedError()
-#
-# class StrategyPI(StrategyInterface):
-#     def __init__(self, Kp=10, Ki=0.1, integral_limit=65535):
-#         self.Kp = Kp
-#         self.Ki = Ki
-#         self.integral = 0.0
-#         self.integral_limit = integral_limit
-#         self.max_output = 2**16
-#
-#     def update(self, current_value, target):
-#         error = target - current_value
-#         P = error * self.Kp
-#
-#         self.integral += error * self.Ki
-#         self.integral = max(min(self.integral, self.integral_limit), 0)
-#
-#         output = P + self.integral
-#
-#         output = max(min(output, self.max_output), 0)
-#
-#         return output
-#
-# class ThermalController:
-#     def __init__(self, temperature_sensor:TempSensorInterface, heater:HeaterInterface, strategy:StrategyInterface, target=0, update_freq=10):
-#         self.sensor = temperature_sensor
-#         self.actuator = heater
-#         self.strategy = strategy
-#         self.target = target
-#         self.timer = Timer()
-#         self.timer.init(freq=update_freq, callback=self.update)
-#
-#     def set_target(self, new_target):
-#         self.target = new_target
-#
-#     def update(self, t):
-#         sensor_reading = self.sensor.read()
-#         strategy_output = self.strategy.update(sensor_reading, self.target)
-#         self.actuator.write(strategy_output)
-#
-# class FreqSensorInterface:
-#     @property
-#     def frequency(self):
-#         raise NotImplementedError()
-#
-# class FreqSensorPulseCounter(FreqSensorInterface):
-#     def __init__(self, pin, measure_freq=1.0):
-#         self.pin = Pin(pin, Pin.IN)
-#         self.pin.irq(handler=self.count_pulse_callback, trigger=Pin.IRQ_RISING)
-#         self.timer = Timer()
-#         self.measure_freq = measure_freq
-#         # O timer dispara a cada segundo
-#         self.timer.init(freq=int(measure_freq), callback=self.finish_callback) 
-#         self.pulse_counter = 0
-#         self._freq = 0
-#
-#     @property
-#     def frequency(self):
-#         return self._freq
-#
-#     def finish_callback(self, t):
-#         # Calcula freq (Hz) = Pulsos / tempo de medição (1/measure_freq)
-#         self._freq = self.pulse_counter * self.measure_freq
-#         self.pulse_counter = 0
-#
-#     def count_pulse_callback(self, pin):
-#         self.pulse_counter += 1
-#
-#
 temp_sensor = SensorThermistorRseriesV2(THERMISTOR_ADC_PIN, R_SERIES)
 freq_counter = FreqSensorPIO(FREQUENCY_PIN, count_to=100_000)
 heater = HeaterPWM(HEATER_PWM_PIN, pwm_freq=60, initial_value=0)
 controller_strategy = StrategyOnOff() 
+fake_oscilator = Clock(FAKE_OSCILATOR_PIN, 455_000)
 
 thermal_controller = ThermalController(
     temp_sensor, 
@@ -292,6 +175,12 @@ def serve(connection):
                 client.close()
 
 if __name__ == "__main__":
+    from time import sleep
+
+    for _ in range(20):
+        print(f"freq:{freq_counter.frequency}\ttemp:{temp_sensor.read()}")
+        sleep(1)
+
     try:
         ip = connect()
         connection = open_socket(ip)
